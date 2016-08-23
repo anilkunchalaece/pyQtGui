e3550f9ddf33d803e4dd5a1909e441f624461c41
Kunchala Anil <anilkunchalaece@gmail.com>
2016-08-23 21:34:00 +0530

    Simple Window Application

--- guiver1.py
+++ guiver1.py
@@ -1,2 +1,25 @@
 #Author : Kunchala Anil
 #Date : Aug 23 2016
+"""
+Iam using python 2.7 with pyQt4
+Download the pyQt4 from https://www.riverbankcomputing.com/software/pyqt/download
+Note : Download the Binary package
+
+"""
+
+import sys
+from PyQt4 import QtGui
+
+def main():
+	app=QtGui.QApplication(sys.argv)
+	
+	w=QtGui.QWidget()
+	w.resize(250,250)
+	w.move(200,200)
+	w.setWindowTitle('Prepare 2 PG')
+	w.show()
+	
+	sys.exit(app.exec_())
+	
+if __name__ == '__main__':
+	main()
